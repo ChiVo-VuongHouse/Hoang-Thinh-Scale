5f12aa9876b5e27c550f4aae18fc60bab9b38d42
ChiVo <vo.chi.210@gmail.com>
2019-11-30 12:17:21 +0700

    Khoi tao test cac component, them route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NewsComponent } from './news/news.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  { path: 'san-pham', component: ProductsComponent },
+  { path: 'tin-tuc', component: NewsComponent },
+  
+  //Route cho nhung link k tim thay
+  {path: '404', component: NotfoundComponent},
+  {path: '**', redirectTo: '/404'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NewsComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
